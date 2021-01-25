c75e0f656c98f6bf8c2a87340b54c0c40ee10699
Abisola-Morohunfolu <amorohunfolu@gmail.com>
2021-01-25 14:36:29 +0100

    made page responsive

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log(gsap);
+
+const tl = gsap.timeline();
+
+tl.from('.nav', { duration: 0.9, opacity: 0, y: -100, ease: 'power.inOut' })
+	.from('.heading-1', { duration: 1.2, y: 300, opacity: 0, ease: 'power.in', delay: -0.9 })
+	.to('.cover-text', {
+		duration: 0.9,
+		width: 0,
+		left: '100%',
+		ease: 'sine.inOut',
+	});
